5e1da46ab64efbafb6f538a9f966054d1db457ef
Hanuman Chandrasekhar <chandu.sekhar@gmail.com>
2020-09-28 10:51:49 +0530

    Create App.kt

---
+++ app/src/main/java/imageapp/com/mobiosafe/App.kt
@@ -0,0 +1,31 @@
+package imageapp.com.mobiosafe
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class App : Application() {
+    public final var CHANNEL_ID = "exampleServiceChannel"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            var serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Example Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        val manager = getSystemService(
+            NotificationManager::class.java
+        )
+        manager.createNotificationChannel(serviceChannel)
+        }
+    }
+}
